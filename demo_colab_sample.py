34cd1b7727d355fae70a89592e815d54bb83e9bd
contractorwolf <contractorwolf@hotmail.com>
2019-08-31 22:07:43 -0400

    added addtl input params and removed the while()

--- demo_colab_sample.py
+++ demo_colab_sample.py
@@ -16,24 +16,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument("-e", "--enc_model_fpath", type=Path, 
-                        default="encoder/saved_models/pretrained.pt",
-                        help="Path to a saved encoder")
-    parser.add_argument("-s", "--syn_model_dir", type=Path, 
-                        default="synthesizer/saved_models/logs-pretrained/",
-                        help="Directory containing the synthesizer model")
-    parser.add_argument("-v", "--voc_model_fpath", type=Path, 
-                        default="vocoder/saved_models/pretrained/pretrained.pt",
-                        help="Path to a saved vocoder")
-    parser.add_argument("--low_mem", action="store_true", help=\
-        "If True, the memory used by the synthesizer will be freed after each use. Adds large "
-        "overhead but allows to save some GPU memory for lower-end GPUs.")
-    parser.add_argument("--no_sound", action="store_true", help=\
-        "If True, audio won't be played.")
+    parser.add_argument("-e", "--enc_model_fpath", type=Path,default="encoder/saved_models/pretrained.pt",help="Path to a saved encoder")
+    parser.add_argument("-s", "--syn_model_dir", type=Path,default="synthesizer/saved_models/logs-pretrained/",help="Directory containing the synthesizer model")
+    parser.add_argument("-v", "--voc_model_fpath", type=Path,default="vocoder/saved_models/pretrained/pretrained.pt",help="Path to a saved vocoder")
+    parser.add_argument("--low_mem", action="store_true", help="If True, the memory used by the synthesizer will be freed after each use. Adds large overhead but allows to save some GPU memory for lower-end GPUs.")
+    parser.add_argument("--no_sound", action="store_true", help="If True, audio won't be played.")
+    parser.add_argument("--out", action="store_true", help="sets the output wav file")
+    parser.add_argument("--textin", action="store_true", help="sets the output wav file")
+
     args = parser.parse_args()
     print_args(args, parser)
     if not args.no_sound:
         import sounddevice as sd
+
+    if args.textin:
+        print("\nargs.textin: ", args.textin)
+    else:
+        print("\nNO VALUE: args.textin")   
         
     
     ## Print some environment information (for debugging purposes)
@@ -140,53 +139,54 @@ if __name__ == '__main__':
     print("Created the embedding")
 
     # ********************************
+    try:
+        ## Generating the spectrogram
+        text = input("Write a sentence (+-20 words) to be synthesized:\n")
+        #text = args.textin 
 
-    print("Interactive generation loop")
-    num_generated = 0
-    while True:
-        try:
 
+        # The synthesizer works in batch, so you need to put your data in a list or numpy array
+        texts = [text]
+        embeds = [embed]
+        # If you know what the attention layer alignments are, you can retrieve them here by
+        # passing return_alignments=True
+        specs = synthesizer.synthesize_spectrograms(texts, embeds)
+        spec = specs[0]
+        print("Created the mel spectrogram")
+        
+        
+        ## Generating the waveform
+        print("Synthesizing the waveform:")
+        # Synthesizing the waveform is fairly straightforward. Remember that the longer the
+        # spectrogram, the more time-efficient the vocoder.
+        generated_wav = vocoder.infer_waveform(spec)
+        
+        
+        ## Post-generation
+        # There's a bug with sounddevice that makes the audio cut one second earlier, so we
+        # pad it.
+        generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
+        
+        # Play the audio (non-blocking)
+        if not args.no_sound:
+            sd.stop()
+            sd.play(generated_wav, synthesizer.sample_rate)
             
-            
-            ## Generating the spectrogram
-            text = input("Write a sentence (+-20 words) to be synthesized:\n")
-            
-            # The synthesizer works in batch, so you need to put your data in a list or numpy array
-            texts = [text]
-            embeds = [embed]
-            # If you know what the attention layer alignments are, you can retrieve them here by
-            # passing return_alignments=True
-            specs = synthesizer.synthesize_spectrograms(texts, embeds)
-            spec = specs[0]
-            print("Created the mel spectrogram")
-            
-            
-            ## Generating the waveform
-            print("Synthesizing the waveform:")
-            # Synthesizing the waveform is fairly straightforward. Remember that the longer the
-            # spectrogram, the more time-efficient the vocoder.
-            generated_wav = vocoder.infer_waveform(spec)
-            
-            
-            ## Post-generation
-            # There's a bug with sounddevice that makes the audio cut one second earlier, so we
-            # pad it.
-            generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
-            
-            # Play the audio (non-blocking)
-            if not args.no_sound:
-                sd.stop()
-                sd.play(generated_wav, synthesizer.sample_rate)
-                
-            # Save it on the disk
-            fpath = "demo_output_%02d.wav" % num_generated
-            print(generated_wav.dtype)
-            librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
-                                     synthesizer.sample_rate)
-            num_generated += 1
-            print("\nSaved output as %s\n\n" % fpath)
-            print("\nSaved output as /content/Real-Time-Voice-Cloning/%s\n\n" % fpath)
-            
-        except Exception as e:
-            print("Caught exception: %s" % repr(e))
-            print("Restarting\n")
+        # Save it on the disk
+        #fpath = "demo_output_%02d.wav" % num_generated
+        fpath = "output.wav"
+
+
+        print(generated_wav.dtype)
+        librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
+                                    synthesizer.sample_rate)
+        num_generated += 1
+        print("\nSaved output as %s\n\n" % fpath)
+        print("\nSaved output as /content/Real-Time-Voice-Cloning/%s\n\n" % fpath)
+        print("\nSaved output as %s\n\n" % args.out)
+        print("\nDONE")
+
+        
+    except Exception as e:
+        print("Caught exception: %s" % repr(e))
+        print("Restarting\n")
