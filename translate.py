9169e7803aa4a96c8e1b47dc4c9669c23decc8a1
contractorwolf <contractorwolf@hotmail.com>
2019-09-04 23:13:15 -0400

    removed low_mem refernce because it didnt have a default

--- translate.py
+++ translate.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     ## Load the models one by one.
     print("Preparing the encoder, the synthesizer and the vocoder...")
     encoder.load_model(args.enc_model_fpath)
-    synthesizer = Synthesizer(args.syn_model_dir.joinpath("taco_pretrained"), low_mem=args.low_mem)
+    synthesizer = Synthesizer(args.syn_model_dir.joinpath("taco_pretrained"))
     vocoder.load_model(args.voc_model_fpath)
 
 
