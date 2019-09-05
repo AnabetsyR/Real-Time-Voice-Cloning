04f2540cf5ca1a5c4b28c74b873cef568b42540a
contractorwolf <contractorwolf@hotmail.com>
2019-09-04 22:50:33 -0400

    added translate.py as a mini version of the code

--- translate.py
+++ translate.py
@@ -10,7 +10,6 @@ import argparse
 import torch
 import sys
 
-
 if __name__ == '__main__':
     ## Info & args
     parser = argparse.ArgumentParser(
@@ -19,20 +18,16 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--enc_model_fpath", type=Path,default="encoder/saved_models/pretrained.pt",help="Path to a saved encoder")
     parser.add_argument("-s", "--syn_model_dir", type=Path,default="synthesizer/saved_models/logs-pretrained/",help="Directory containing the synthesizer model")
     parser.add_argument("-v", "--voc_model_fpath", type=Path,default="vocoder/saved_models/pretrained/pretrained.pt",help="Path to a saved vocoder")
-    parser.add_argument("--low_mem", action="store_true", help="If True, the memory used by the synthesizer will be freed after each use. Adds large overhead but allows to save some GPU memory for lower-end GPUs.")
-    parser.add_argument("--no_sound", action="store_true", help="If True, audio won't be played.")
+
     parser.add_argument("--out", type=Path,default="output.wav", help="sets the output wav file")
     parser.add_argument("--textin", type=Path,help="sets the output wav file")
-    parser.add_argument("--voicein", type=Path,default="sample.wav",  help="sets the input wav file")
+    parser.add_argument("--voicein", type=Path,default="input.wav",  help="sets the input wav file")
 
     args = parser.parse_args()
     print_args(args, parser)
-    #if not args.no_sound:
-    #    print("\nNO SOUND SD")
-    #    import sounddevice as sd
         
     if args.textin:
-            print("\nargs.textin: ", args.textin)
+        print("\nargs.textin: ", args.textin)
     else:
         print("\nNO VALUE: args.textin")   
         
@@ -71,57 +66,7 @@ if __name__ == '__main__':
     encoder.load_model(args.enc_model_fpath)
     synthesizer = Synthesizer(args.syn_model_dir.joinpath("taco_pretrained"), low_mem=args.low_mem)
     vocoder.load_model(args.voc_model_fpath)
-    
-    
-    ## Run a test
-    print("Testing your configuration with small inputs.")
-    # Forward an audio waveform of zeroes that lasts 1 second. Notice how we can get the encoder's
-    # sampling rate, which may differ.
-    # If you're unfamiliar with digital audio, know that it is encoded as an array of floats 
-    # (or sometimes integers, but mostly floats in this projects) ranging from -1 to 1.
-    # The sampling rate is the number of values (samples) recorded per second, it is set to
-    # 16000 for the encoder. Creating an array of length <sampling_rate> will always correspond 
-    # to an audio of 1 second.
-    print("\tTesting the encoder...")
-    encoder.embed_utterance(np.zeros(encoder.sampling_rate))
-    
-    # Create a dummy embedding. You would normally use the embedding that encoder.embed_utterance
-    # returns, but here we're going to make one ourselves just for the sake of showing that it's
-    # possible.
-    embed = np.random.rand(speaker_embedding_size)
-    # Embeddings are L2-normalized (this isn't important here, but if you want to make your own 
-    # embeddings it will be).
-    embed /= np.linalg.norm(embed)
-    # The synthesizer can handle multiple inputs with batching. Let's create another embedding to 
-    # illustrate that
-    embeds = [embed, np.zeros(speaker_embedding_size)]
-    texts = ["test 1", "test 2"]
-    print("\tTesting the synthesizer... (loading the model will output a lot of text)")
-    mels = synthesizer.synthesize_spectrograms(texts, embeds)
-    
-    # The vocoder synthesizes one waveform at a time, but it's more efficient for long ones. We 
-    # can concatenate the mel spectrograms to a single one.
-    mel = np.concatenate(mels, axis=1)
-    # The vocoder can take a callback function to display the generation. More on that later. For 
-    # now we'll simply hide it like this:
-    no_action = lambda *args: None
-    print("\tTesting the vocoder...")
-    # For the sake of making this test short, we'll pass a short target length. The target length 
-    # is the length of the wav segments that are processed in parallel. E.g. for audio sampled 
-    # at 16000 Hertz, a target length of 8000 means that the target audio will be cut in chunks of
-    # 0.5 seconds which will all be generated together. The parameters here are absurdly short, and 
-    # that has a detrimental effect on the quality of the audio. The default parameters are 
-    # recommended in general.
-    vocoder.infer_waveform(mel, target=200, overlap=50, progress_callback=no_action)
-    
-    print("All test passed! You can now synthesize speech.\n\n")
-    
-    
-    ## Interactive speech generation
-    print("This is a GUI-less example of interface to SV2TTS. The purpose of this script is to "
-          "show how you can interface this project easily with your own. See the source code for "
-          "an explanation of what is happening.\n")
-    
+
 
     # ********************************
     if args.voicein:
@@ -185,19 +130,14 @@ if __name__ == '__main__':
         # pad it.
         generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
         
-        # Play the audio (non-blocking)
-        #if not args.no_sound:
-        #    print("\nNO SOUND STOP")
-        #    sd.stop()
-        #    sd.play(generated_wav, synthesizer.sample_rate)
-            
         # Save it on the disk
         if args.out:
             fpath = args.out
         else:
             fpath = "output.wav"
 
-        print(generated_wav.dtype)
+        print("generated_wav.dtype: ", generated_wav.dtype)
+
         librosa.output.write_wav(fpath, generated_wav.astype(np.float32), synthesizer.sample_rate)
 
         print("\nSaved output as %s\n\n" % fpath)
